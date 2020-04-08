266fc7cfe719f54d2989b134f9cb8a6968e5e392
Shamilee Peruvaludhi <34155550+shamilee05@users.noreply.github.com>
2020-04-08 20:56:39 +0530

    Added details for interact_with_db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express'); // Require Express
 const app = express(); // Create the app, basically the website
 var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
 
 app.listen(3000, () => {
     console.log("Listening at the port 3000")
@@ -11,6 +12,7 @@ app.listen(3000, () => {
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false })); // For POST requests
 app.use(bodyParser.json()); //For POST requests
+app.use(cookieParser());
 
 // Temporary database for now
 const database = {
@@ -18,6 +20,13 @@ const database = {
     'Domestic Activities': {option1: 'Using the geyser', option2: 'Using the kettle'},
     'Work Activities': {option1: 'Using the laptop', option2: 'Using the printer'}
 };
+
+var myLogger = function (req, res, next) {
+    console.log('We will be displaying activities now.');
+    next();
+}
+  
+app.use(myLogger);
 
 app.get('/activities', (req,res) => {
     const allActivities = Object.keys(database);
@@ -38,26 +47,26 @@ app.get('/activities/:activityID', (req,res) => {
     }
 });
 
+// var requestChecker = require('./middleware');
+
+// // Add the middleware to express app 
+// app.use(requestChecker);
+
+// // Another middleware will get executed after above one.
+// // Order of middleware is sequential.
+// app.use(function(req,res) {
+//     res.send('Hello there!');
+// });
+
+
 // For MongoDB
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const UserSchema = new Schema({
-    firstname: String,
-    lastname: String,
-    username: String,
-    password: String
-});
-
-// users is the name of the database
-var UserModel = mongoose.model('UserModel', UserSchema, 'users');
-
 const url = "mongodb://localhost:27017/users";
 
 // Legacy code - Not needed with Mongoose5
 // mongoose.Promise = global.Promise;
 
-mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true },function(err,db){
+mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },function(err,db) {  //Connect to the 'users' database using local MongoDB
     if(err) {
         console.log("Error " + err);
     }
@@ -65,10 +74,31 @@ mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true },functio
         console.log("Connected on url " + url);
 } });
 
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    firstname: String,
+    lastname: String,
+    username: String,
+    password: String,
+    tokens:[{
+		access : {
+			type : String,
+			required : true
+		},
+		token : {
+			type : String,
+			required : true
+		}
+	}]
+});
+
+// 'users' is the name of the database
+var UserModel = mongoose.model('UserModel', UserSchema, 'users');
+
 // Creating a new user
 app.post('/register_new_user', function(req,res) { 
     console.log("Creating a new user");
-    var newUser = new UserModel();
+    var newUser = new UserModel();      //When creating a new user(document), always use the model and not the schema
     newUser.firstname = req.body.fname; 
     newUser.lastname = req.body.lname; 
     newUser.username = req.body.uname; 
@@ -76,7 +106,7 @@ app.post('/register_new_user', function(req,res) {
     
     // console.log(req.body.uname);
 
-    newUser.save(function(err, insertedDoc){
+    newUser.save(function(err, users) {
         if(err) {
             console.log("Error "+err);
         } else {
@@ -85,13 +115,183 @@ app.post('/register_new_user', function(req,res) {
     })
 });
 
+// Reading the database and displaying all users' data
+app.get('/display_all_users',function(req,res) {
+    console.log('Show all users');
+    UserModel.find({})
+    .exec(function(err, users){
+        if(err) {
+            console.log("Error "+err);
+        } else {
+            res.json(users);
+        }
+    }); 
+});
+
+// Reading the database and displaying data for a particular user
+app.get('/display_user/:uname',function(req,res) {
+    console.log('Showing user for the given username');
+    //console.log(req.params.uname);
+    UserModel.findOne({ username: req.params.uname})
+    .exec(function(err, user_part){
+        if(err) {
+            console.log("Error "+err);
+        } else {
+            res.json(user_part);
+        }
+    }); 
+});
+
+// Updating a user's data
+app.put('/update_user/:uname',function(req,res) { 
+    console.log('Updating user details');
+    console.log(req.body.newpwd);
+    UserModel.findOneAndUpdate({ username: req.params.uname}, { password: req.body.newpwd }, function(err,updateDoc) {
+        if(err) {
+            console.log("Error "+err);
+        }
+        else {
+            res.send("User password updated successfully");
+        } 
+    })
+});
+
+// Deleting a user from the database
+app.get('/delete_user/:uname',function(req,res) {
+    console.log('Deleting this user');
+    UserModel.deleteOne({ username: req.params.uname }, function (err) {
+        if(err) {
+            console.log("Error "+err);
+        } else {
+            res.send("User deleted successfully");
+        }   
+    });
+});
+
 // Verifying user before logging in 
 app.post('/login_user', function(req,res) { 
-    console.log("Thodi si dhool meri");
-    var found_user = UserModel.findOne({username: req.body.uname});
+    UserModel.findOne({ username: req.body.uname },(err,retrived_user)=> {
+            if(retrived_user.password === req.body.pwd) {
+                console.log("Successful Login");
+                res.redirect("home.html");
+            }
+            else {
+                console.log("Incorrect Password");
+                res.send("Incorrect Password. Please enter the correct password.");
+            }
+        }    
+    );
+});
 
-    if(found_user) {
-        console.log(found_user.name);
-        res.end();
-    }
+//Creating a RESTful web service which involves dealing with the data of users and involving JWT tokens
+
+// Load the full build.
+var _ = require('lodash');
+
+// Load the FP build for immutable auto-curried iteratee-first data-last methods.
+var fp = require('lodash/fp');
+ 
+// Load method categories.
+var array = require('lodash/array');
+var object = require('lodash/fp/object');
+ 
+// Cherry-pick methods for smaller browserify/rollup/webpack bundles.
+var at = require('lodash/at');
+var curryN = require('lodash/fp/curryN');
+
+// Generating an Auth Token for a new user
+UserSchema.methods.generateAuthToken = function () {
+	var user = this;
+	var access = 'authorized';
+	var token = jwt.sign({_id : user._id.toHexString(), access}, 'carbon_counter').toString();
+	user.tokens = user.tokens.concat( [{access , token}]);
+
+	return user.save().then(() => {
+		return token;
+	});
+};
+
+// Find a user using their assigned Auth Token
+UserSchema.statics.findByToken = function(token) {
+	var User = this;
+	var decoded;
+
+	try{
+		decoded = jwt.verify(token , 'carbon_counter');
+	}
+	catch(e) {
+        // Do nothing
+	}
+
+	return User.findOne({
+		'_id' : decoded._id,
+		'tokens.token' : token,
+		'tokens.access' : 'carbon_counter'
+	});
+};
+
+// Creating a new user: POST (Create)
+app.post('/users', (req,res) => {
+	var body = _.pick(req.body, ['firstname','lastname','username','password']);
+	var user = new UserModel(body);
+
+	user.save().then(() => {
+		return user.generateAuthToken();    // Generate an AuthToken for the new user
+	}).then((token) => {
+		res.header('x-auth',token).send(user);  // Add token header to the response
+	}).catch( (err) =>{
+		res.status(400).send(err);
+	});
+});
+
+// Reading info about the users: GET (READ)
+app.get('/users', (req,res) => {
+	UserModel.find().then( (users) => {
+		res.send(users);
+	}, (err) => {
+		res.status(400).send(err);
+	});
+});
+
+// Updating user information
+app.patch('/users/:id', (req,res) => {
+	var id = req.params.id;
+    var body = _.pick(req.body, ['firstname','lastname','username','password']);
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	UserModel.findByIdAndUpdate(id,{$set : body},{new : true}).then( (user) =>{
+		if(!user)
+			return res.status(404).send();
+
+		res.send({user});
+
+	}).catch((err) =>{
+		res.status(400).send();
+	})
+});
+
+// Deleting a user
+app.delete('/users/:id', (req,res) => {
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	User.findByIdAndRemove(id).then((user) => {
+		if(!user)
+			return res.status(404).send();
+
+		res.send(user);
+	}).catch( (err) => {
+		res.status(400).send();
+	});
+});
+
+// Route not found (404)
+app.use(function(req, res) {
+    return res.status(404).send({ message: 'Route'+req.url+' Not found.' });
 });
