780108f6604633876623bd9a9d3b5eacc43d6aa0
Shamilee Peruvaludhi <34155550+shamilee05@users.noreply.github.com>
2020-04-11 14:28:10 +0530

    Only for Heroku

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/activities/:activityID', (req,res) => {
 
 // For MongoDB
 const mongoose = require('mongoose');
-const url = "mongodb://localhost:27017/users" || "mongodb+srv://cluster0-2pnd0.mongodb.net/test?retryWrites=true&w=majority";
+const url = /* "mongodb://localhost:27017/users" || */ "mongodb+srv://cluster0-2pnd0.mongodb.net/test?retryWrites=true&w=majority";
 
 // Legacy code - Not needed with Mongoose5
 // mongoose.Promise = global.Promise;
